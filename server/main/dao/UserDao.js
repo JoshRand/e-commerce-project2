49ac0e90a17fc725cc41102989539fc3f045ae7d
Joshua anderson <Mp4ct@live.com>
2020-10-19 15:40:42 -0500

    Implemented User update route, updated user add, and changed schema for users

--- server/main/dao/UserDao.js
+++ server/main/dao/UserDao.js
@@ -22,34 +22,92 @@ class UserDao
     {
 
     }
-
     testFunction()
     {
         console.log("heyya");
     }
     getUsers()
     {
+       
         return new Promise((resolve,reject)=>{
+            try {
+                con.query('select * from users',(err,rows) => {
+                    // if(err)
+                    //     throw err;
+                    console.log("data from db:");
+                   resolve(rows);
+                });
+            } catch (error) {
+                
+            }
+            
 
-            con.query('select * from users',(err,rows) => {
-                if(err)
-                    throw err;
-                console.log("data from db:");
-               resolve(rows);
-            });
-
-         
      });
     }
-    addUser(uname){
-        var msg = 'insert users(username) value(\''+uname+'\')';
+    addUser(username,password,role){
+        var msg = 'insert users(username,password,role) value(\''+username+'\',\''+password+'\',\''+role+'\')';
+       try {
         con.query(msg,(err,rows) => {
+            
+            //     throw err;
             if(err)
-                throw err;
-            console.log("added to db");
+                console.log("duplicate username or error");
+            else
+                console.log("added to db");
          
         });
+       } catch (error) {
+           
+       }
     }
 
+    updateUser(userId, userName, password, repassword, role){
+        try {
+            con.query('select * from users where userId=\''+userId+'\';',(err,rows) => {
+                console.log("data from db:");
+                var uname;
+                var pass;
+                var rol;
+                if(userName === "" || userName === rows[0].userName)
+                {
+                    uname = rows[0].userName;
+                }
+                else
+                    uname = userName;
+                if(password === "" || password === rows[0].password)
+                {
+                    pass = rows[0].password;
+                }
+                else
+                    pass = password;
+                if(role === "" || role === rows[0].role)
+                {
+                    rol = rows[0].role;
+                }
+                else
+                    rol = role;
+                if(password === repassword)
+                {
+                
+                    var msg = 'update users set userName=\''+uname+'\', password=\''+pass+'\',role=\''+rol+'\' where userId=\''+userId+'\';';
+                    con.query(msg,(err,rows) => {
+                        if(err)
+                            console.log("error updating user");
+                        else
+                            console.log("successfully updated user");
+                    });
+                }
+                else
+                {
+                    console.log("a user with this name exists");
+                }
+                
+        
+            });
+        } catch (error) {
+            
+        }
+        
+    }
 }
 export default UserDao;
--- server/main/server.js
+++ server/main/server.js
@@ -1,25 +1,50 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import UserDao from './dao/UserDao.js';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 var app = express();
 var userDao = new UserDao();
 var userList = [];
-var paramss;
+
+app.use(cookieParser());
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
 
 app.get('/users', function (req, res) {
     
     userDao.getUsers().then(function(rows) {
         res.send(rows);
-        console.log(rows);
-        console.log(rows[0].username);
+
     }).catch((err) => setImmediate(() => { throw err; }));
-    
+
 })
+
 app.use(bodyParser.json());
 app.post('/user',function (req, res) {
-    userDao.addUser(req.body.username);
+    userDao.addUser(req.body.username, req.body.password, req.body.role);
     res.send(req.body)
   })
+app.patch('/user',function (req, res)
+{   
+    userDao.updateUser(req.body.userId, req.body.userName, req.body.password,req.body.repassword, req.body.role);
+    res.send(req.body);
+})
+
+app.post('/login')
 
 var server = app.listen(8081, function () {
    var host = server.address().address
